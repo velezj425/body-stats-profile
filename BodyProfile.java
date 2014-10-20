f31de93ce47019cc349413ea2c26afd225b80a69
Julian Velez <velezj425@users.noreply.github.com>
2014-10-19 21:32:33 -0400

    Update BodyProfile.java

--- BodyProfile.java
+++ BodyProfile.java
@@ -96,6 +96,15 @@ public class BodyProfile {
 	}
 	
 	/**
+	 * getGender returns the user's gender
+	 * @return The user's gender
+	 */
+	 
+	public String getGender() {
+		return gender;
+	}
+	
+	/**
 	 * Returns the user's height
 	 * @return The user's height
 	 */
@@ -132,6 +141,15 @@ public class BodyProfile {
 	}
 	
 	/**
+	 * sets the bmi to a double value
+	 * @param newBMI The value to set
+	 */
+	 
+	public void setBMI(double newBMI) {
+		bmi = newBMI;
+	}
+	
+	/**
 	 * Calculates and returns the user's 
 	 * Body Mass Index (BMI)
 	 */
@@ -157,59 +175,56 @@ public class BodyProfile {
 	public void saveInfo() throws IOException {
 		PrintWriter file = new PrintWriter("Profile");
 		
-		file.println("\t\t" + name + "'s Profile");
+		file.println("Name : " + name);
 		file.println("Age : " + age);
-		file.println("Height : " + height);
-		file.println("Weight : " + weight);
+		file.println("Gender : " + gender);
+		file.println("Height(in) : " + height);
+		file.println("Weight(lbs) : " + weight);
 		file.printf("BMI : %.1f\n", getBMI());
-		file.printf("Body Fat : %.1f\n", getBodyFat());
+		file.printf("Body Fat Percentage: %.1f\n", getBodyFat());
 		
 		file.close();
 	}
+	
+	/**
+	 * sets the body fat field to a specified double
+	 * @param newBodyFat The value to set
+	 */
+	 
+	public void setBodyFat(double newBodyFat) {
+		bodyFat = newBodyFat;
+	}
 
 	/**
-	 * Calculates the user's body fat percentage
+	 * Calculates the user's body fat percentage (male)
 	 */
 	
-	public void setBodyFat() {
-		Scanner keyboard = new Scanner(System.in);
-		
-		if(gender.equals("MALE")) {
-			// Gather measurement
-			System.out.print("Enter your waist girth(inches): ");
-			double girth = keyboard.nextDouble();
-			
+	public void setBodyFatMale(double girth) {	
 			// Make calculation
 			double result1 = (weight * 1.082) + 94.42;
 			double leanBodyMass = result1 - (girth * 4.15);
-			bodyFat = ((weight - leanBodyMass) * 100) / weight;
-		}
-		else {
-			// Gather measurements
-			System.out.print("Enter the circumference of your wrist(inches): ");
-			double wristCircum = keyboard.nextDouble();
-			System.out.print("Enter the cirumference of your waist(inches): ");
-			double waistCircum = keyboard.nextDouble();
-			System.out.print("Enter the circumference of your hips(inches): ");
-			double hipCircum = keyboard.nextDouble();
-			System.out.print("Enter the circumference of your forearm(inches: ");
-			double forearmCircum = keyboard.nextDouble();
-			
-			// make calculations
-			double result1 = weight * 0.732;
-			double result2 = result1 + 8.987;
-			double result3 = wristCircum / 3.14;
-			double result4 = waistCircum * 0.157;
-			double result5 = hipCircum * 0.249;
-			double result6 = forearmCircum * 0.434;
-			double result7 = result2 + result3;
-			double result8 = result7 - result4;
-			double result9 = result8 - result5;
-			double leanBodyMass = result6 + result9;
-			
-			bodyFat = ((weight - leanBodyMass) * 100) / weight;
-			
-		}
+			bodyFat = ((weight - leanBodyMass) * 100) / weight;		
+	}
+	
+	/**
+	 * Calculates the user's body fat percentage (female)
+	 */
+	 
+	public void setBodyFatFemale(double wristCircum, double waistCircum, 
+			double hipCircum, double forearmCircum) {
+		// make calculations
+		double result1 = weight * 0.732;
+		double result2 = result1 + 8.987;
+		double result3 = wristCircum / 3.14;
+		double result4 = waistCircum * 0.157;
+		double result5 = hipCircum * 0.249;
+		double result6 = forearmCircum * 0.434;
+		double result7 = result2 + result3;
+		double result8 = result7 - result4;
+		double result9 = result8 - result5;
+		double leanBodyMass = result6 + result9;
+		
+		bodyFat = ((weight - leanBodyMass) * 100) / weight;
 	}
 	
 	/**
